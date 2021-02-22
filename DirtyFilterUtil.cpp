b3724f484c08a376e2b5b7cbc9e20d20312e957d
zhz <gh_zhz@ghgame.cn>
2021-02-22 11:00:51 +0800

    test3

--- DirtyFilterUtil.cpp
+++ DirtyFilterUtil.cpp
@@ -11,17 +11,6 @@ using namespace rapidjson;
 typedef  unsigned short     uint16_t;
 typedef unsigned int        uint32_t;
 
-static const unsigned char BASE64_ENC_MAP[64] =
-{
-    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
-    'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
-    'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd',
-    'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
-    'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
-    'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7',
-    '8', '9', '+', '/'
-};
-
 
 
 
@@ -269,3 +258,14 @@ int test2base64_encode(const unsigned char *in,
     *out_len = output_len;
     return 0;
 }
+
+static consttesa3 unsigned char BASE64_ENC_MAP[64] =
+{
+    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+    'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
+    'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd',
+    'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+    'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
+    'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7',
+    '8', '9', '+', '/'
+};
